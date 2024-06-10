b29840a130816b60f1f1ad775fc8b7e063068818
phamt2 <phamt2@berea.edu>
2024-06-10 12:16:14 -0500

    Added new prompts

--- Experiments/Typicality/data/prompts.py
+++ Experiments/Typicality/data/prompts.py
@@ -27,15 +27,19 @@ def generate_prompts(members, random_member_typical, random_member_atypical, loc
     base_name = os.path.basename(loc)
     category, _ = os.path.splitext(base_name)
     
-    typical_str = ", ".join(random_member_typical)
-    atypical_str = ", ".join(random_member_atypical)
+    list_of_birds = (random_member_atypical + random_member_typical + ['magpie'])
+    random.shuffle(list_of_birds)
+    # typical_str = ", ".join(random_member_typical)
+    # atypical_str = ", ".join(random_member_atypical)
+    list_of_birds = ", ".join(list_of_birds)
     
     rank_present = 1
     rank_absent = 1
 
     # First handle the 'present' stimulus
     for member in members:
-        preamble = f"A list includes {typical_str}, {atypical_str}. In the list {member} is"
+        preamble = f"Following is a list that contains a number of birds. After the list, a bird will be judged as either present or absent in the list. If the list contains bird A, bird A should be said to be present. If the list does not contain the bird, answer with absent. The list of birds is: {list_of_birds}. According to the list, magpie is present. According to the list, kingfisher is absent. According to the list, {member} is "
+        # preamble = f"A list includes {typical_str}, {atypical_str}. In the list {member} is"
         
         if member in random_member_typical or member in random_member_atypical:
             true_category = 'present'
@@ -47,7 +51,8 @@ def generate_prompts(members, random_member_typical, random_member_atypical, loc
 
     # Then handle the 'absent' stimulus
     for member in members:
-        preamble = f"A list includes {typical_str}, {atypical_str}. In the list {member} is"
+        preamble = f"Following is a list that contains a number of birds. After the list, a bird will be judged as either present or absent in the list. If the list contains bird A, bird A should be said to be present. If the list does not contain the bird, answer with absent. The list of birds is: {list_of_birds}. According to the list, magpie is present. According to the list, kingfisher is absent. According to the list, {member} is "
+        # preamble = f"A list includes {typical_str}, {atypical_str}. In the list {member} is"
         
         if member in random_member_typical or member in random_member_atypical:
             true_category = 'present'
@@ -72,7 +77,7 @@ def main():
     # Get name for resulting CSV:
     base_name = os.path.basename(loc)
     name,_ = os.path.splitext(base_name)
-    output_file = name + '_result.csv'
+    output_file = name + '_prompts.csv'
     output_df = pd.DataFrame(data, columns=['Preamble', 'Stimulus', 'True_Category', 'Item', 'Rank', 'Category'])
     
     # Save to CSV
@@ -82,3 +87,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# Following is a list that contains a number of birds. After the list, a bird will be judged as either present or absent in the list. If the list contains bird A, bird A should be said to be present. If the list does not contain bird A, bird A should be said to be absent. ###<bird list>$$$ According to the list, <bird> is ______
